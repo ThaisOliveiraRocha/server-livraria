e4f2d65f0366dcd123bc651f1ec59a8bb5c1b8af
Thais Rocha <thais.oliveirarocha@hotmail.com>
2020-03-11 15:32:48 -0300

    Faz busca e inserção no banco

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,39 @@
-//conexão com o banco
+const mongo = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017";
+
+const item = {
+  imagem: "book2.png",
+  titulo: "Tarde Demais",
+  genero: "romance",
+  autor: "Colleen Hoover",
+  ano: "2014",
+  situacao: "novo",
+  preco: "10.50",
+  qtd: 0
+};
+
+mongo.connect(url, (err, client) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const dataBase = client.db("LivrariaDB");
+  const collection = dataBase.collection("Produtos");
+  return listAll(collection);
+//  insertItem(dataBase, item);
+});
+
+listAll = (collection) => {
+  collection.find().toArray((err, items) => {
+    console.log(items);
+
+    return items;
+  });
+};
+
+insertItem = bd => {
+  bd.collection("Produtos").insertOne(item);
+  console.log("item inserido.");
+};
+module.exports = mongo;
